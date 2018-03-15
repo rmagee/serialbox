13ec93215568b2a71b44d9bc3451a389b58c6d7e
Serial Lab <slab@serial-lab.com>
2018-03-15 16:55:47 -0600

    Added auto package detection to setup.

--- setup.py
+++ setup.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import re
-import shutil
-import sys
-
-from setuptools import setup
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+from setuptools import setup, find_packages
 
 
 def get_package_data(package):
@@ -31,25 +19,6 @@ def get_package_data(package):
                           for filename in filenames])
     return {package: filepaths}
 
-
-if sys.argv[-1] == 'publish':
-    if os.system("pip freeze | grep wheel"):
-        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
-        sys.exit()
-    if os.system("pip freeze | grep twine"):
-        print("twine not installed.\nUse `pip install twine`.\nExiting.")
-        sys.exit()
-    os.system("python setup.py sdist bdist_wheel")
-    os.system("twine upload dist/*")
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %s -m 'version %s'" % (version, version))
-    print("  git push --tags")
-    shutil.rmtree('dist')
-    shutil.rmtree('build')
-    shutil.rmtree('serialbox.egg-info')
-    sys.exit()
-
-
 setup(
     name='serialbox',
     version='1.0.7',
@@ -57,8 +26,8 @@ setup(
     license='GPL',
     description='Serial Number Distribution Made Easy.',
     author='Rob Magee',
-    author_email='rob@serial-lab.com',
-    packages=get_packages('serialbox'),
+    author_email='slab@serial-lab.com',
+    packages=find_packages(),
     package_data=get_package_data('serialbox'),
     install_requires=['Django', 'djangorestframework', 'djangorestframework-csv',
                       'djangorestframework-xml', 'Markdown', 'decorator', 'six',
