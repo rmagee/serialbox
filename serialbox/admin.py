272ddb74819b695bc6481cd8a82aff55a10afb2e
Serial Lab <slab@serial-lab.com>
2019-11-22 09:36:23 -0700

    Updated model admins.

--- serialbox/admin.py
+++ serialbox/admin.py
@@ -3,17 +3,32 @@ from django.contrib import admin
 from list_based_flavorpack.models import ListBasedRegion
 from random_flavorpack.models import RandomizedRegion
 
+
+class ResponseRule(admin.ModelAdmin):
+    list_display = ('pool', 'rule', 'content_type')
+    search_fields = ('pool',)
+
+
+class Response(admin.ModelAdmin):
+    list_display = ('pool', 'type',
+                    'size_granted', 'remote_host')
+    search_fields = ('remote_host', 'pool')
+
+
 class RegionInline(admin.StackedInline):
     model = models.SequentialRegion
     extra = 0
+
 
 class ListBasedInline(admin.StackedInline):
     model = ListBasedRegion
     extra = 0
 
+
 class RandomRegionInline(admin.StackedInline):
     model = RandomizedRegion
     extra = 0
+
 
 class PoolAdmin(admin.ModelAdmin):
     list_display = (
@@ -23,6 +38,7 @@ class PoolAdmin(admin.ModelAdmin):
     )
     search_fields = ['readable_name', 'machine_name']
     inlines = [RegionInline, ListBasedInline, RandomRegionInline]
+
 
 def register_to_site(admin_site):
     admin_site.register(models.Pool, PoolAdmin)
