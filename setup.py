c02d27ad3b091760f809f48bacf2e2094fb76cde
Serial Lab <slab@serial-lab.com>
2020-06-06 11:09:55 -0600

    Bump version: 2.1.1 → 2.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ readme = open('README.rst').read()
 
 setup(
     name='serialbox',
-    version='2.1.1',
+    version='2.2.0',
     url='http://www.serial-lab.com',
     license='GPL',
     long_description=readme,
