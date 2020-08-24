bc933b52b2901c7882426f87a331b4612d332e08
Serial Lab <slab@serial-lab.com>
2020-08-24 16:01:05 -0600

    updated requirements

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ readme = open('README.rst').read()
 
 setup(
     name='serialbox',
-    version='2.4.0',
+    version='2.2.0',
     url='http://www.serial-lab.com',
     license='GPL',
     long_description=readme,
