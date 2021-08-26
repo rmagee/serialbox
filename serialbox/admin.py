e5eba5fc5e2da75717c06f504239edfdba24f7e7
Serial Lab <slab@serial-lab.com>
2021-08-26 12:06:25 -0600

    Refined the admin for display and created a read only model for requests.

--- serialbox/admin.py
+++ serialbox/admin.py
@@ -2,16 +2,30 @@ from serialbox import models
 from django.contrib import admin
 from list_based_flavorpack.models import ListBasedRegion
 from random_flavorpack.models import RandomizedRegion
+from django.utils.safestring import mark_safe
 
 class ResponseRuleAdmin(admin.ModelAdmin):
     list_display = ('pool', 'rule', 'content_type')
     search_fields = ('pool__readable_name','pool__machine_name')
 
-
+@admin.register(models.Response)
 class ResponseAdmin(admin.ModelAdmin):
+    def task(self):
+        if self.task_name != '' and self.task_name is not None:
+            return mark_safe('<a class="download-task" href="%(url)s%(task_name)s/">%(task_name)s</a>' %
+                             {'url': '/capture/task-data/', 'task_name': self.task_name})
+        else:
+            return ''
+
     list_display = ('pool', 'type',
-                    'size_granted', 'remote_host')
+                    'size_granted', 'remote_host', task)
     search_fields = ('response',)
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + \
+               [field.name for field in obj._meta.fields] + \
+               [field.name for field in obj._meta.many_to_many]
+
 
 
 class RegionInline(admin.StackedInline):
